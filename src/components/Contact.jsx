d82fb7be96c9752416808eed8372c09bd5bad62c
[Dominik] <domidi3326@gmail.com>
2024-12-06 14:54:18 +0100

    changed contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,12 @@
+import {CONTACT} from "../constants"
+
 const Contact = () => {
     return (
         <div className="border-b border-neutral-900 pb-20">
             <h1 className="my-10 text-center text-4xl">Contact</h1>
+            <div className="text-center tracking-tighter">
+                <a className="border-b">{CONTACT.email}</a>
+            </div>
         </div>
     );
 };
