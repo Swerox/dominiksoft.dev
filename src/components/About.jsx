2d1cd8f5765528a455c67377a8d1c58dd359530a
Swerox <domidi3326@gmail.com>
2024-12-08 14:06:17 +0100

    added some animations

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,46 +1,116 @@
 import { ABOUT_TEXT } from "../constants";
-import { RiReactjsLine } from "react-icons/ri"
-import { DiPython } from "react-icons/di"
-import { SiFlask } from "react-icons/si"
-import { DiJavascript } from "react-icons/di"
-import { FaSwift } from "react-icons/fa6"
+import { RiReactjsLine } from "react-icons/ri";
+import { DiPython } from "react-icons/di";
+import { SiFlask } from "react-icons/si";
+import { DiJavascript } from "react-icons/di";
+import { FaSwift } from "react-icons/fa6";
+import { FaNodeJs } from "react-icons/fa";
 
+import { motion } from "framer-motion";
 
+const iconAnimations = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const About = () => {
   return (
     <div className="min-h-screen">
-      <h1 className="my-20 text-center text-4xl pt-11">
+      <motion.h1
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1 }}
+        className="my-20 text-center text-4xl pt-11"
+      >
         About
         <span className="text-neutral-500"> Me</span>
-      </h1>
+      </motion.h1>
       <div className="flex flex-wrap">
         <div className="w-full">
           <div className="flex justify-center text-center lg:justify-start">
-            <p className="my-2 maxw-xl py-6">{ABOUT_TEXT}</p>
+            <motion.p
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1, delay: 0.5 }}
+              className="my-2 lg:maxw-xl py-6 lg:px-80 sm:px-12"
+            >
+              {ABOUT_TEXT}
+            </motion.p>
           </div>
         </div>
       </div>
-            <h1 className="my-20 text-center text-4xl">Technologies i use</h1>
-            <div className="flex flex-wrap items-center justify-center gap-4">
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <RiReactjsLine className ="text-7xl text-cyan-400"></RiReactjsLine>
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <DiJavascript className ="text-7xl text-yellow-400"></DiJavascript>
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <SiFlask className ="text-7xl text-green"></SiFlask>
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <DiPython className ="text-7xl"></DiPython>
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <FaSwift className ="text-7xl text-orange-600"></FaSwift>
-                </div>
-            </div>
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: 100 }}
+        transition={{ duration: 1, delay: 0.9 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies i use
+      </motion.h1>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 0.5, delay: 1 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          variants={iconAnimations(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400 hover:scale-110"></RiReactjsLine>
+        </motion.div>
+        <motion.div
+          variants={iconAnimations(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <DiJavascript className="text-7xl text-yellow-400 hover:scale-110"></DiJavascript>
+        </motion.div>
+        <motion.div
+          variants={iconAnimations(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiFlask className="text-7xl text-green hover:scale-110"></SiFlask>
+        </motion.div>
+        <motion.div
+          variants={iconAnimations(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <DiPython className="text-7xl hover:scale-110"></DiPython>
+        </motion.div>
+        <motion.div
+          variants={iconAnimations(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaSwift className="text-7xl text-orange-600 hover:scale-110"></FaSwift>
+        </motion.div>
+        <motion.div
+          variants={iconAnimations(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaNodeJs className="text-7xl text-green-500 hover:scale-110"></FaNodeJs>
+        </motion.div>
+      </motion.div>
     </div>
-    
   );
 };
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,7 @@
 import { FaGithub } from "react-icons/fa";
-import TypeWriter from 'typewriter-effect'
+import TypeWriter from "typewriter-effect";
+import { motion } from "framer-motion";
+
 const Hero = () => {
   return (
     <div className="min-h-screen lg:mb-35">
@@ -7,29 +9,48 @@ const Hero = () => {
         <div className="w-full">
           <nav className="mb-20 flex items-center justify-between px-6 py-6">
             <div className="text-3xl">
-              <h1>dominiksoft.dev</h1>
+              <motion.h1
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ duration: 1 }}
+              >
+                dominiksoft.dev
+              </motion.h1>
             </div>
-            <div className="text-3xl">
-            <a 
-            href="https://github.com/Swerox"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="underline">
-              <FaGithub />
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 50 }}
+              transition={{ duration: 1 }}
+              className="text-3xl"
+            >
+              <a
+                href="https://github.com/Swerox"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline"
+              >
+                <FaGithub />
               </a>
-            </div>
+            </motion.div>
           </nav>
 
-          <div className="flex flex-col items-center lg:items-start justify-center h-full px-6">
-            <h1 className="pb-11 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+          <div className="flex flex-col lg:items-start h-full px-6">
+            <motion.h1
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1, delay: 0.5 }}
+              className="pb-11 text-5xl lg:text-8xl md:text-4xl font-thin tracking-tight lg:mt-16"
+            >
               Dominik Dittrich
-            </h1>
-            <span className="bg-gradient-to-r from-violet-500 via-slate-500 to-purple-400 bg-clip-text text-5xl tracking-tight text-transparent">
-              <TypeWriter onInit={(typewriter) => {
-                typewriter.typeString('Software Developer')
-                .start();
-              }}>
-              </TypeWriter>
+            </motion.h1>
+            <span className="text-3xl lg:text-5xl md:text-4xl bg-gradient-to-r from-violet-500 via-slate-500 to-purple-400 bg-clip-text tracking-tight text-transparent">
+              <TypeWriter
+                onInit={(typewriter) => {
+                  typewriter.typeString("Software Developer")
+                    .pauseFor(500)
+                    .start();
+                }}
+              ></TypeWriter>
             </span>
           </div>
         </div>
